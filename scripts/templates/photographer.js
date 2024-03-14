8d01f9a1404eea77e688d1d30e955224820e5f8c
ShumiFr <gagnaire.pauline@gmail.com>
2024-03-14 16:18:20 +0100

    Initialisation du projet et utilisation de fetch pour récupérér les images.

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,29 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const div = document.createElement("div");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+
+    a.setAttribute("href", "photographer.html");
+    a.setAttribute("class", "photographer_card");
+    div.setAttribute("class", "photographer_img__container");
+    img.setAttribute("src", picture);
+
+    h2.textContent = name;
+
+    article.appendChild(a);
+    article.appendChild(div);
+    div.appendChild(img);
+    a.appendChild(div);
+    a.appendChild(h2);
+
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
